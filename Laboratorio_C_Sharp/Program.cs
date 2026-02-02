5b2dc1933752d475fb8f96c210c7dd8a3efe88a8
Emorto <emilio.ortiz.tobarra@gmail.com>
2026-02-02 12:53:40 +0100

    Agregar archivos de proyecto.

---
+++ Laboratorio_C_Sharp/Program.cs
@@ -0,0 +1,51 @@
+﻿using Laboratorio_C_Sharp.Temas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.Clear();
+                Console.WriteLine("=== PORTAFOLIO DE APRENDIZAJE C# ===");
+                Console.WriteLine("1. Estructuras Condicionales (If/Else)");
+                Console.WriteLine("2. Ciclos (While/For)");
+                Console.WriteLine("0. Salir");
+                Console.Write("\nSelecciona un tema para probar: ");
+                string opcion = Console.ReadLine();
+               
+                    switch (opcion)
+                    {
+                        case "1":
+                            Condicionales.Ejecutar(); // Llamada a la clase externa
+                            break;
+                        case "2":
+                            // Ciclos.Ejecutar(); 
+                            break;
+                        case "0":
+                            continuar = false;
+                            break;
+                        default:
+                            Console.WriteLine("Opción inválida.");
+                            break;
+                    }
+                
+
+                if (continuar)
+                {
+                    Console.WriteLine("\nPresiona cualquier tecla para volver al menú...");
+                    Console.ReadKey();
+                }
+            }
+        }
+        
+    }
+}
---
+++ Laboratorio_C_Sharp/Temas/Condicionales.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_C_Sharp.Temas
+{
+    public static class Condicionales
+    {
+        /// <summary>
+        /// Método para ejecutar el tema de estructuras condicionales.
+        /// </summary>
+        public static void Ejecutar()
+        {
+            Console.Clear();
+            Console.WriteLine("=== TEMA: ESTRUCTURAS CONDICIONALES ===");
+            Console.Write("Introduce la nota del alumno: ");
+
+            if (double.TryParse(Console.ReadLine(), out double nota))
+            {
+                string resultado = (nota >= 7) ? "Aprobado" : "Suspenso";
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("Entrada no válida.");
+            }
+        }
+    }
+}
